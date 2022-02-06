9892f4a22349553d6ffa6f1b58de602883beedd2
cborse <cgborsellino@gmail.com>
2022-02-06 12:19:44 -0500

    helper

--- models/creature.go
+++ models/creature.go
@@ -1,5 +1,7 @@
 package models
 
+import "math"
+
 type Creature struct {
 	ID        uint64 `db:"id" json:"id"`
 	UserID    uint64 `db:"user_id" json:"user_id"`
@@ -16,4 +18,18 @@ type Creature struct {
 	Skill1    uint32 `db:"skill1" json:"skill1"`
 	Skill2    uint32 `db:"skill2" json:"skill2"`
 	Skill3    uint32 `db:"skill3" json:"skill3"`
+}
+
+func getXPAtLevel(level int) uint32 {
+	// total xp = floor(713/20736*A3^3+2*A3^2-A3)
+	return uint32(float64(713)/float64(20736)*math.Pow(float64(level), 3) + 2*math.Pow(float64(level), 2) - float64(level))
+}
+
+func (c *Creature) GetLevel() int {
+	for i := 2; i <= 144; i++ {
+		if c.XP < getXPAtLevel(i) {
+			return i - 1
+		}
+	}
+	return 144
 }
